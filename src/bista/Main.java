484beaad8d644e05240554460454ed7061344d24
ikermani15 <iker.maniega@gmail.com>
2025-02-17 20:41:29 +0100

    Main bista sortu eta proba

--- src/bista/Main.java
+++ src/bista/Main.java
@@ -2,11 +2,19 @@ package bista;
 
 import java.awt.EventQueue;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
 
 public class Main extends JFrame {
 
 	private static final long serialVersionUID = 1L;
+	private JPanel panelFondo;
+	private JLabel lblBackground;
+	private JPanel panelTitulo;
+	private JLabel lblNewLabel;
 
 	/**
 	 * Launch the application.
@@ -28,9 +36,29 @@ public class Main extends JFrame {
 	 * Create the frame.
 	 */
 	public Main() {
-		setBounds(100, 100, 450, 300);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+		initialize();
 	}
 
+	private void initialize() {
+		setBounds(400, 200, 650, 425);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().add(getPanelFondo(), BorderLayout.CENTER);
+	}
+
+	private JPanel getPanelFondo() {
+		if (panelFondo == null) {
+			panelFondo = new JPanel();
+
+			String imagePath = "/irudiak/back.png";
+			ImageIcon imageIcon = new ImageIcon(getClass().getResource(imagePath));
+			panelFondo.setLayout(new BorderLayout(0, 0));
+
+			// JLabel sortu irudia agertzeko
+			lblBackground = new JLabel(imageIcon);
+
+			panelFondo.add(lblBackground);
+		}
+		return panelFondo;
+	}
 }
